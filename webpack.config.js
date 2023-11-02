7dcc5bfa64ab7b67d43091f57a01e22355e962f1
Anna <annaushakova471@gmail.com>
2023-11-03 03:18:16 +0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+import path from 'path';
+import url from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+export default {
+  mode: 'development',
+  target: 'web',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/'
+  },
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        use: [{
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true
+          }
+        }]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    alias: {
+      "@": path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new HtmlWebpackPlugin({ 
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+    new webpack.ProgressPlugin()
+  ],
+}
